96ead0e00d3e49382c01f12a1afaffcca0d155f9
ret-Phoenix <phoenixusa@yandex.ru>
2016-06-03 14:42:43 +0300

    start

---
+++ SelectValueSharp/SelectValueSharp/MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: savage
+ * Date: 30.05.2016
+ * Time: 22:07
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace SelectValueSharp
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.TextBox textBoxSearchValue;
+		private System.Windows.Forms.ListBox listBoxAllValues;
+		private System.Windows.Forms.ListBox listBoxResult;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBoxSearchValue = new System.Windows.Forms.TextBox();
+			this.listBoxAllValues = new System.Windows.Forms.ListBox();
+			this.listBoxResult = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// textBoxSearchValue
+			// 
+			this.textBoxSearchValue.Dock = System.Windows.Forms.DockStyle.Top;
+			this.textBoxSearchValue.Location = new System.Drawing.Point(0, 0);
+			this.textBoxSearchValue.Name = "textBoxSearchValue";
+			this.textBoxSearchValue.Size = new System.Drawing.Size(499, 20);
+			this.textBoxSearchValue.TabIndex = 0;
+			this.textBoxSearchValue.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			this.textBoxSearchValue.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox1KeyDown);
+			// 
+			// listBoxAllValues
+			// 
+			this.listBoxAllValues.FormattingEnabled = true;
+			this.listBoxAllValues.Location = new System.Drawing.Point(12, 43);
+			this.listBoxAllValues.Name = "listBoxAllValues";
+			this.listBoxAllValues.Size = new System.Drawing.Size(266, 82);
+			this.listBoxAllValues.TabIndex = 1;
+			this.listBoxAllValues.Visible = false;
+			// 
+			// listBoxResult
+			// 
+			this.listBoxResult.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listBoxResult.FormattingEnabled = true;
+			this.listBoxResult.Location = new System.Drawing.Point(0, 20);
+			this.listBoxResult.Name = "listBoxResult";
+			this.listBoxResult.Size = new System.Drawing.Size(499, 299);
+			this.listBoxResult.TabIndex = 2;
+			this.listBoxResult.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ListBox2KeyDown);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(499, 319);
+			this.Controls.Add(this.listBoxResult);
+			this.Controls.Add(this.listBoxAllValues);
+			this.Controls.Add(this.textBoxSearchValue);
+			this.Name = "MainForm";
+			this.Text = "SelectValueSharp";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ SelectValueSharp/SelectValueSharp/MainForm.cs
@@ -0,0 +1,121 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: savage
+ * Date: 30.05.2016
+ * Time: 22:07
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace SelectValueSharp
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		string fileNameOut;
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+			//int x = 0;
+			if (textBoxSearchValue.Text == string.Empty) {
+				listBoxResult.Items.AddRange(listBoxAllValues.Items);
+				return;
+			}
+			
+			string match = @".*(" + textBoxSearchValue.Text + @").*";
+			match = match.Replace(" ", @".*");
+			listBoxResult.Items.Clear();
+			
+			Regex newReg = new Regex(match, RegexOptions.IgnoreCase);
+			
+			// Examine text box length 
+			if (textBoxSearchValue.Text.Length != 0) {
+				foreach (var strv in listBoxAllValues.Items) {
+					if (newReg.IsMatch(strv.ToString())) {
+						listBoxResult.Items.Add(strv);
+						listBoxResult.SetSelected(0, true);
+					}
+				}
+			}
+		}
+	
+		void ListBox2KeyDown(object sender, KeyEventArgs e)
+		{
+			if (Keys.Enter == e.KeyCode) {
+				// MessageBox.Show("2: " + listBox2.SelectedItem.ToString());
+				string сurValue = listBoxResult.SelectedItem.ToString();
+//				if (сurValue == string.Empty) {
+//					сurValue = textBoxSearchValue.Text;
+//				}
+				ResultToFile(сurValue);
+				Application.Exit();
+			} else if (Keys.Left == e.KeyCode) {
+				textBoxSearchValue.Text = listBoxResult.SelectedItem.ToString();
+				textBoxSearchValue.Focus();
+			} else if (Keys.Right == e.KeyCode) {
+				textBoxSearchValue.Text = listBoxResult.SelectedItem.ToString();
+				textBoxSearchValue.Focus();
+			}
+		}
+
+		void TextBox1KeyDown(object sender, KeyEventArgs e)
+		{
+			if (Keys.Down == e.KeyCode) {
+				listBoxResult.Select();
+			} else if (Keys.Enter == e.KeyCode) {
+				string сurValue = textBoxSearchValue.Text;
+				ResultToFile(сurValue);
+				Application.Exit();
+			}
+		}
+		
+		void ResultToFile(string valueToFile) {
+			StreamWriter sw = new StreamWriter(fileNameOut, false,System.Text.Encoding.Default);
+			//sw.Write(valueToFile, System.Text.Encoding.GetEncoding(1251));
+			sw.Write(valueToFile);
+			sw.Close();
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			string[] appArgs = System.Environment.GetCommandLineArgs();
+			
+//			foreach (string strVal in appArgs) {
+//				listBox2.Items.Add(strVal);
+//			}
+			
+			string line;
+			string fileName = appArgs[1]; //"c:\\work\\portable\\v8CfgAddsAhk\\tmp\\module.1s";
+			fileNameOut = fileName;
+			FileStream sss = File.Open(fileName, FileMode.Open, FileAccess.Read);
+			if (sss != null) {
+				StreamReader reader = new StreamReader(sss, System.Text.Encoding.GetEncoding(1251));
+				while ((line = reader.ReadLine()) != null) {
+					listBoxAllValues.Items.Add(line);
+					listBoxResult.Items.Add(line);
+                    	
+				}
+				sss.Close();
+			}
+		}
+	}
+}
